5f01dd2fec7c6dc0bde05d339a02d0f737be57a6
Mario Latifi <MARIO.LATIFI@local.ittcesena.it>
2024-12-14 09:38:34 +0100

    tutto senza test

---
+++ ConsoleStruttureDati/Program.cs
@@ -0,0 +1,96 @@
+﻿using StruttureDati;
+using System.Diagnostics;
+
+namespace ConsoleStruttureDati
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Creazione di un array di 1.000 di interi compresi tra 1e 100000");
+            IntArray valoriMille = new IntArray(1000, 0, 100000);
+           
+            Console.WriteLine("Creazione di un array di 1.000.000 di interi compresi tra 1e 100000");
+            IntArray valoriMilione = new IntArray(1000000, 0, 1000000);
+
+            Console.WriteLine("Creazione di un array di 1.000.000.000 di interi compresi tra 1e 100000");
+            IntArray valoriMiliardo = new IntArray(1000000, 0, 1000000000);
+
+            Console.WriteLine("------------SEARCH---------------");
+
+            //tiemr per calcolo dei tempi di esecuzione
+            Stopwatch watch;
+            long elapsedMs;
+            Console.WriteLine("ricerca del valore 100 su array da 1000 elementi...");
+            //inizio del timer per calcolare il tempo di esecuzione del metodo
+            watch = Stopwatch.StartNew();
+            int? index1 = valoriMille.Search(100);            
+            //stop del timer
+            watch.Stop();
+            //calcolo del tempo di esecuzione in secondi
+            elapsedMs = watch.ElapsedMilliseconds;
+            
+            Console.WriteLine($"il valore 100 su array da 1000 elementi si trova all'indice {index1}");
+            Console.WriteLine($"tempo di esecuzione {elapsedMs} millisecondi");
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("ricerca del valore 100 su array da 1000000 di elementi...");
+            watch = Stopwatch.StartNew();
+            int? index2 = valoriMilione.Search(100);
+            watch.Stop();
+            //calcolo del tempo di esecuzione in secondi
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"il valore 100 su array da 1000000 elementi si trova all'indice {index2}");
+            Console.WriteLine($"tempo di esecuzione {elapsedMs} millisecondi");
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("ricerca del valore 100 su array da 1000000000 di elementi...");
+            watch = Stopwatch.StartNew();
+            int? index3 = valoriMiliardo.Search(100);
+            watch.Stop();
+            //calcolo del tempo di esecuzione in secondi
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"il valore 100 su array da 1000000000 elementi si trova all'indice {index3}");
+            Console.WriteLine($"tempo di esecuzione {elapsedMs} millisecondi");
+
+
+            Console.WriteLine("------------SORT---------------");
+            
+            Console.WriteLine("ordinamento array da 1000 elementi con Selection Sort");
+            watch = Stopwatch.StartNew();
+            valoriMille.SelectionSort();
+            watch.Stop();
+            //calcolo del tempo di esecuzione in secondi
+            elapsedMs = watch.ElapsedMilliseconds;           
+            Console.WriteLine($"tempo di esecuzione {elapsedMs} millisecondi");
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("ordinamento array da 1000000 elementi con Selection Sort");
+            watch = Stopwatch.StartNew();
+            valoriMilione.SelectionSort();
+            watch.Stop();
+            //calcolo del tempo di esecuzione in secondi
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"tempo di esecuzione {elapsedMs} millisecondi");
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("ordinamento array da 1000000000 elementi con Selection Sort");
+            watch = Stopwatch.StartNew();
+            valoriMiliardo.SelectionSort();
+            watch.Stop();
+            //calcolo del tempo di esecuzione in secondi
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"tempo di esecuzione {elapsedMs} millisecondi");
+
+            Console.WriteLine("---------------------------");
+            */
+            IntArray valori = new IntArray(20, 0, 100);
+            valori.BubbleSort();
+            IntArray valori2 = new IntArray(20, 0, 100);
+            valori2.BubbleSortWithSentinel();
+        }
+    }
+}
---
+++ StruttureDati/IntArray.cs
@@ -0,0 +1,191 @@
+﻿namespace StruttureDati
+{
+    public class IntArray
+    {
+        int[] array;
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+
+        public IntArray(int numberOfElement) 
+        {
+            array = new int[numberOfElement];
+        }
+
+        public IntArray(int[] array)
+        {
+            this.array = array;
+        }
+
+        /// <summary>
+        /// array of random between min e max
+        /// </summary>
+        /// <param name="numberOfElement"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public IntArray(int numberOfElement, int min, int max) :this(numberOfElement)
+        {
+            Random rnd = new Random();
+
+            for(int i=0; i<array.Length; i++)
+            {
+                array[i]= rnd.Next(min,max);
+            }
+        }
+
+        /// <summary>
+        /// array con valori tra 0 e length ordinato i disordinati
+        /// </summary>
+        /// <param name="numberOfElement"></param>
+        /// <param name="ordered"></param>
+        public IntArray(int numberOfElement, bool ordered) : this(numberOfElement)
+        {
+            Random rnd = new Random();
+            if (ordered)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = i;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rnd.Next(array.Length); ;
+                }
+            }
+        }
+
+        
+
+        public int ElementAt(int index)
+        {
+            try
+            {
+                return array[index];
+            }
+            catch (Exception e)
+            {
+                throw new Exception("ElementAt Error: " + e.Message);
+            }
+        }
+
+        public void InsertElementAt(int index, int value)
+        {
+            try
+            {
+                array[index] = value;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("InsertElementAt Error: " + e.Message);
+            }
+        }
+
+        public void BubbleSortWithSentinel()
+        {
+            int lastSwap = array.Length-1;
+            int timeSwap = array.Length - 1;
+            int swapIndex = 0;
+            while (lastSwap != 0)
+            {
+                for (int i = 0; i < lastSwap; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                        timeSwap= i;
+                    }
+                }
+                if (timeSwap == lastSwap) { lastSwap = 0; }
+                lastSwap = timeSwap;
+            }
+        }
+
+
+        public void BubbleSort()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)// -1 perchè sennò va in stack overflow
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public void SelectionSort()
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                // cerco il minimo
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (array[j] < array[min_idx])
+                        min_idx = j;
+
+                // Scambio
+                int temp = array[min_idx];
+                array[min_idx] = array[i];
+                array[i] = temp;
+            }
+        }
+
+        /// <summary>
+        /// search element in array (first occurrence)
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns>index of elment or null</returns>
+        public int? Search(int element)
+        {
+            for(int i=0; i<array.Length; i++)
+            {
+                if (array[i] == element)
+                    return i;
+            }
+
+            return null;
+
+        }
+
+        /// <summary>
+        /// search element in array
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns>index of elment or null</returns>
+        public int? BinarySearch(int element)
+        {
+            int start = 0, end = array.Length - 1, middle = 0;
+
+            while (start <= end)
+            {
+                middle = (start + end) / 2;
+                if (element < array[middle])
+                {
+                    end = middle - 1;
+                }
+                else
+                {
+                    if (element > array[middle])
+                        start = middle + 1;
+                    else
+                        return middle;
+                }
+            }
+            return null;
+        }
+
+    }
+}
